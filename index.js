c4d8fab21604a89e4f4906a75cf1c476984b66b7
MobuchiElly <ugwueleazer@gmail.com>
2024-07-02 10:45:49 +0100

    career-page

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ UTCcurrentTime.forEach(value => {
 const image = document.getElementById("image");
 const {naturalWidth, naturalHeight, width, height} = image;
 width !== 0 || height !== 0 ? window.alert("The natural dimension of the image property has been altered") : null; 
-console.log(`natural width: ${naturalWidth}\nnatural height: ${naturalHeight}\nwidth: ${width}\nheight: ${height}`)
+

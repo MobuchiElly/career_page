561652f9dbd0e6f8bb0c80e7267f3a45dfefdf61
MobuchiElly <ugwueleazer@gmail.com>
2024-07-02 22:20:56 +0100

    ui update

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const UTCcurrentTime = document.querySelectorAll(".UTC-current-time");
-const currentDay = document.querySelectorAll(".current-day");
+// const UTCcurrentTime = document.querySelectorAll(".UTC-current-time");
+// const currentDay = document.querySelectorAll(".current-day");
 
-const days = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-]
-const date = new Date(); 
-currentDay.forEach((value) => {
-    value.innerText = days[date.getDay()];
-});
-UTCcurrentTime.forEach(value => {
-    value.innerText = date.toUTCString();
-})
+// const days = [
+//     "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+// ]
+// const date = new Date(); 
+// currentDay.forEach((value) => {
+//     value.innerText = days[date.getDay()];
+// });
+// UTCcurrentTime.forEach(value => {
+//     value.innerText = date.toUTCString();
+// })
+
+
+const image = document.getElementById("image")
+console.log(image.naturalWidth, image.naturalHeight)
+console.log(image.clientWidth, image.clientHeight);

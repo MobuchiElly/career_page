588f36eb83e5338ef922896543410e90bed29c4d
MobuchiElly <ugwueleazer@gmail.com>
2024-07-02 11:03:26 +0100

    career-page

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ UTCcurrentTime.forEach(value => {
 //Check to ensure the naturalWidth and naturalHeight are not altered
 const image = document.getElementById("image");
 const {naturalWidth, naturalHeight, width, height} = image;
-width !== 0 || height !== 0 ? window.alert("The natural dimension of the image property has been altered") : null; 
+naturalWidth === 0 || naturalHeight === 0 ? window.alert("The natural dimension of the image property has been altered") : null;
 

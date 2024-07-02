f6d5e1fabf4c24bdc719671b6c9664591a906835
MobuchiElly <ugwueleazer@gmail.com>
2024-07-02 22:28:14 +0100

    code review

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-// const UTCcurrentTime = document.querySelectorAll(".UTC-current-time");
-// const currentDay = document.querySelectorAll(".current-day");
+const UTCcurrentTime = document.querySelectorAll(".UTC-current-time");
+const currentDay = document.querySelectorAll(".current-day");
 
-// const days = [
-//     "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-// ]
-// const date = new Date(); 
-// currentDay.forEach((value) => {
-//     value.innerText = days[date.getDay()];
-// });
-// UTCcurrentTime.forEach(value => {
-//     value.innerText = date.toUTCString();
-// })
+const now = new Date();
+const utcTime = now.toISOString();
+
+const days = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+]
+const date = new Date(); 
+currentDay.forEach((value) => {
+    value.innerText = days[date.getDay()];
+});
+UTCcurrentTime.forEach(value => {
+    value.innerText = utcTime;
+})
 
 
 const image = document.getElementById("image")
